68289cc745a3b17f54f7234151168f1b138eb7eb
Bruno <psbrunosouza@gmail.com>
2022-11-29 14:59:26 -0300

    feat: implements tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+  DetailsViewService, FileManager,
+  MenuClickEventArgs,
+  NavigationPaneService,
+  ToolbarService,
+  ViewType
+} from "@syncfusion/ej2-angular-filemanager";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers: [ NavigationPaneService, ToolbarService, DetailsViewService]
 })
-export class AppComponent {
-  title = 'project-example-syncfusion';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  @ViewChild('fileManager') fileManagerInstance!: FileManager;
+  view: ViewType = "Details"
+  ajaxSettings = {
+    url: 'https://60bbd2283a39900017b2df38.mockapi.io/cookit/api/v1/files',
+  };
+
+  contextMenuSettings = {
+    file: ['Custom', 'Open', '|', 'Delete', 'Rename', '|', 'Details'],
+    folder: ['Custom', 'Open', '|', 'Delete', 'Rename', '|', 'Details'],
+    layout: ['Custom', 'SortBy', 'View', 'Refresh', '|', 'NewFolder', 'Upload', '|', 'Details', '|', 'SelectAll'],
+    visible: true,
+  };
+
+  public ngOnInit(): void {
+    this.view = "Details";
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  menuClick(args: any) {
+    if (args.item.text === 'Custom') {
+      alert('You have clicked custom menu item')
+    }
+  }
+  menuOpen(args: any){
+    this.fileManagerInstance.disableMenuItems(["Custom"]);
+  }
 }
